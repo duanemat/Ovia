3cbb0af81bb638466d497c0ed2faa1868391549b
Matthew Duane <duanemat@gmail.com>
2017-07-22 21:08:38 -0400

    First project commit.

---
+++ app/Http/Controllers/RoomsController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class RoomsController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    //
+}
